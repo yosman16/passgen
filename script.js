18e95a53fdb6371cbbc1d44b2082a31263b27c2b
yosman16 <133923762+yosman16@users.noreply.github.com>
2023-06-03 20:11:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-// Alert message
-var message = "Hello, this is an alert!";
+var capitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Display the alert
-window.onload = function() {
-  alert(message);
-};
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var symbols = ['!', '@', '#', '$', '%', '&', '*', '+', '-', '=', '?', '~'];
+
+
+
+var button = document.getElementById("passwordButton");
+var paragraph = document.getElementById("myParagraph");
+
+function getRandomCharacters(arr, count) {
+  var result = [];
+  for (var i = 0; i < count; i++) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    result.push(arr[randomIndex]);
+  }
+  return result;
+}
+
+// Convert the string into an array of character
+
+
+
+button.addEventListener("click", function() {
+  
+  var a = getRandomCharacters(capitalLetters, 5);
+  var b = getRandomCharacters(lowercaseLetters, 5);
+  var c = getRandomCharacters(numbers, 5);
+  var d = getRandomCharacters(symbols, 5);
+  var e = a + b + c + d
+  var f = e.replace(/,/g, '');
+  
+  function shuffleString(string) {
+  var array = string.split(''); // Convert string to an array of characters
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.join(''); // Convert the shuffled array back to a string
+}
+  
+  var shuffledString = shuffleString(f);
+
+
+  paragraph.textContent = shuffledString;
+});
