2eba9f76967d07320de507c17b639d6f0d6eea36
yosman16 <133923762+yosman16@users.noreply.github.com>
2023-06-07 01:30:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ button.addEventListener("click", function() {
 document.getElementById("copyButton").addEventListener("click", function() {
   const textToCopy = document.getElementById("myParagraph").innerText;
   navigator.clipboard.writeText(textToCopy).then(function() {
-    alert("Text copied to clipboard!");
   }, function(error) {
     console.error("Error copying text: ", error);
   });
